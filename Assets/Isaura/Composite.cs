76ada59771979aeebfbed8c3efbd372c65999085
Keijiro Takahashi <keijiro@gmail.com>
2017-12-03 17:31:15 +0900

    Initial commit

---
+++ Assets/Isaura/Composite.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Isaura
+{
+    public class Composite : MonoBehaviour
+    {
+        [SerializeField] Texture _buffer;
+        [SerializeField] Shader _shader;
+
+        Material _material;
+
+        void OnDestroy()
+        {
+            if (_material != null)
+            {
+                Destroy(_material);
+                _material = null;
+            }
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture dest)
+        {
+            if (_material == null)
+            {
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.DontSave;
+            }
+
+            _material.SetTexture("_BufferTex", _buffer);
+
+            Graphics.Blit(source, dest, _material, 0);
+        }
+    }
+}
---
+++ Assets/Isaura/Marker.shader
@@ -0,0 +1,34 @@
+﻿Shader "Isaura/Marker"
+{
+    SubShader
+    {
+        Pass
+        {
+            ZWrite Off ZTest Always Blend One One
+
+            CGPROGRAM
+
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+
+            #include "UnityCG.cginc"
+
+            half Vertex(
+                float4 position : POSITION,
+                half3 normal : NORMAL,
+                out float4 sv_position : SV_Position
+            ) : COLOR
+            {
+                sv_position = UnityObjectToClipPos(position);
+                return mul(UNITY_MATRIX_V, mul(unity_ObjectToWorld, normal)).z;
+            }
+
+            half4 Fragment(half color : COLOR) : SV_Target
+            {
+                return color;
+            }
+
+            ENDCG
+        }
+    }
+}
